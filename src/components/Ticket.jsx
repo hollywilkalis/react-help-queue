f15727118d323371bb0d2788d5150736bc4131df
Holly Wilkalis <holly.wilkalis@gmail.com>
2018-02-03 22:16:43 -0800

    Add development server tools

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -11,7 +11,7 @@ function Ticket(props){
   );
 }
 
-ticket.propTypes = {
+Ticket.propTypes = {
   names: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
   issue: PropTypes.string
